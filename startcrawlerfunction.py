fae04fa56162638319633ae1bac0a55eb1533b71
mingyu110 <48540798+mingyu110@users.noreply.github.com>
2024-06-04 10:17:31 +0800

    Delete startcrawlerfunction.py

--- startcrawlerfunction.py
+++
@@ -1,8 +0,0 @@
-import json
-import boto3
-
-def lambda_handler(event, context):
-    target = event["crawlername"]
-    glueclient = boto3.client('glue')
-    glueclient.start_crawler(Name=target)
-
